4b59e7d96ba5f9ae1381c2c3b018894feb7ddf40
weihe <187399692@qq.com>
2025-10-24 23:28:24 +0800

    refactor: 更新 iceberg.js 以使用 dist 目录中的 index.js，移除不再需要的构建脚本

--- packages/cli/bin/iceberg.js
+++ packages/cli/bin/iceberg.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { run } = require('../src/index.js');
+const { run } = require('../dist/index.js');
 
 run(process.argv).catch((error) => {
   console.error('Iceberg CLI 运行失败：', error.message);
